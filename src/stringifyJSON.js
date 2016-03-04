e5fd9ad7232d51a9b57704f67fb57747ce6ce558
Ben Rachbach <brachbach@gmail.com>
2016-03-03 19:24:30 -0800

    Made stringify work properly for everything but undefined

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,33 @@
 
 // but you don't so you're going to write it from scratch:
 
+
+//things that this needs to deal with according to the spec: ~all primitives, objects, arrays
+//doesn't need to deal with: functions, undefined
+//[1,2].toString() returns "1,2"
+//object to string returns nonsense
+//things that can't simply be .toString'ed: null, arrays, objects. typeof null = object btw. Start w/ object and drill down.
+//note that .toString of an array gives you the values separated by commas, collapsing any inner structure of nesting
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (typeof obj == "object") {
+  	if (obj === null) {
+  	  return "null";
+  	};
+  	if (Array.isArray(obj)) {
+  	  var content = obj.reduce(function(accumulator,item){
+  	  	return accumulator + stringifyJSON(item) + ","
+  	  },"")
+  	  return "[" + content.slice(0,-1) + "]"
+  	} else {
+  	  var content = "";
+  	  for (key in obj) {
+  	  	content += stringifyJSON(key) + ":" + stringifyJSON(obj[key]) + ",";
+  	  }
+  	  return "{" + content.slice(0,-1) + "}"
+  	}
+  };
+  if (typeof obj == "string") {
+  	return '"' + obj + '"'
+  }
+  return obj.toString()
 };
