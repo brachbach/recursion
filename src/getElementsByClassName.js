5c3270275f920dc871b04d4d40e71d60c6af9c90
Ben Rachbach <brachbach@gmail.com>
2016-03-14 22:10:49 +0300

    Made some comments about what the specs for the function are and explored perverse instantiations of the solution

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,5 +4,13 @@
 // };
 
 // But instead we're going to implement it from scratch:
+
+//returns an array of the parts of the DOM (not jQuery objects) that have the given class name. If a parent
+//and child both have the name, both will be returned.
+
+//equivalent to return $(document).find('.' + className);. Clearly find makes this trivial!
+//however, funny enough, the test suite says that the perverse instantiation fails the test. Not sure why...
+//need to make sure not to commit the changes to the test suite
 var getElementsByClassName = function(className) {
+  return document.getElementsByClassName(className);
 };
