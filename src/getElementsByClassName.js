2f880521f6883ed2427a6b0b99893371eba4e5a9
Ben Rachbach <brachbach@gmail.com>
2016-03-14 21:45:12 +0300

    Deleted my work on getElementsByClassName, because I think I was headed in the wrong direction and was only going to confuse myself.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,16 +5,4 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className) {
-  var elements = document.body.childNodes;
-  var matches = []
-  for (var i = 0; i < elements.length; i++) {
-  	var element = elements[i]
-  	var classes = element.classList;
-  	if (classes) {
-  	  if (classes.indexOf(className) > -1) { // doesn't work b/c classes isn't an array. Probably worth trying a different strategy at this point rather than continuing to mess with these non-array arrays. Also maybe worth looking up what these objects actually are
-  	    matches.push(element);
-  	  };
-  	}  
-  }
-  return matches
 };
